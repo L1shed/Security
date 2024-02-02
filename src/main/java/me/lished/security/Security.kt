49f06ed012901fddf346493145c3bc3a548c891d
BuildTools <unconfigured@null.spigotmc.org>
2024-02-02 21:02:35 +0100

    Java --> Kotlin cuz its waaaaaaaay better

--- src/main/java/me/lished/security/Security.kt
+++ src/main/java/me/lished/security/Security.kt
@@ -1,17 +1,13 @@
-package me.lished.security;
+package me.lished.security
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin
 
-public final class Security extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
+class Security : JavaPlugin() {
+    override fun onEnable() {
         // Plugin startup logic
-
     }
 
-    @Override
-    public void onDisable() {
+    override fun onDisable() {
         // Plugin shutdown logic
     }
 }
