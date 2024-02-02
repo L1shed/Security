59e95baa8b8cee5be9e4d6f15afd1f47b1ad17e0
BuildTools <unconfigured@null.spigotmc.org>
2024-02-02 20:42:24 +0100

    Initial commit

---
+++ src/main/java/me/lished/security/Security.java
@@ -0,0 +1,17 @@
+package me.lished.security;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Security extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
