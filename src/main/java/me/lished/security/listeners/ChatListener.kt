06b4e2440aa2fe0e789590722dac77981e13fab2
BuildTools <unconfigured@null.spigotmc.org>
2024-02-04 12:37:14 +0100

    Added JSON parsing for chat filtering. also added needed dependencies

--- src/main/java/me/lished/security/listeners/ChatListener.kt
+++ src/main/java/me/lished/security/listeners/ChatListener.kt
@@ -1,6 +1,8 @@
 package me.lished.security.listeners
 
 import io.papermc.paper.event.player.AsyncChatEvent
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import net.kyori.adventure.text.Component
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -8,6 +10,9 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+
+@Serializable
+data class Result(val result: String)
 
 class ChatListener : Listener {
     @EventHandler
@@ -19,7 +24,10 @@ class ChatListener : Listener {
         if (connection.responseCode == HttpURLConnection.HTTP_OK) {
             val reader = BufferedReader(InputStreamReader(connection.inputStream))
             val response = reader.readText()
-            e.message(Component.text(response.substring(11, response.length - 2)))
+
+            val result = Json.decodeFromString<Result>(response).result
+
+            e.message(Component.text(result))
         }
     }
 }
