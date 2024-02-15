5a3e38781c10e2a617e3b80f348c23184468fcdd
BuildTools <skywarspanel@gmail.com>
2024-02-15 15:57:10 +0100

    Switch API

--- src/main/java/me/lished/security/managers/AuthManager.kt
+++ src/main/java/me/lished/security/managers/AuthManager.kt
@@ -43,7 +43,7 @@ object AuthManager {
     }
 
     fun hasVPN(player: Player): Boolean {
-        val url = URL("https://vpnapi.io/api/${player.address.hostString}?key=gjjghg")
+        val url = URL("http://ip-api.com/json/${player.address.hostString}")
         val connection = url.openConnection() as HttpURLConnection
         connection.requestMethod = "GET"
 
